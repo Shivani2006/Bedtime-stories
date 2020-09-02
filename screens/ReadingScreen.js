080d0fa71a8cb8f281a1b7f125d018f2615cc67f
Shivani2006 <60694698+Shivani2006@users.noreply.github.com>
2020-09-02 17:14:57 +0530

    uploaded files

---
+++ screens/ReadingScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Text, View, TouchableOpacity,StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class ReadingScreen extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        hasCameraPermissions: null,
+        scanned: false,
+        scannedBookId: '',
+        scannedReaderId:'',
+        buttonState: 'normal'
+      }
+    }
+
+    getCameraPermissions = async (id) =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA);
+      
+      this.setState({
+        /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+        hasCameraPermissions: status === "granted",
+        buttonState: id,
+        scanned: false
+      });
+    }
+
+    handleBarCodeScanned = async({type, data})=>{
+      const {buttonState} = this.state
+
+      if(buttonState==="BookId"){
+        this.setState({
+          scanned: true,
+          scannedBookId: data,
+          buttonState: 'normal'
+        });
+      }
+      else if(buttonState==="ReaderId"){
+        this.setState({
+          scanned: true,
+          scannedStudentId: data,
+          buttonState: 'normal'
+        });
+      }
+      
+    }
+
+    render() {
+      const hasCameraPermissions = this.state.hasCameraPermissions;
+      const scanned = this.state.scanned;
+      const buttonState = this.state.buttonState;
+
+      if (buttonState !== "normal" && hasCameraPermissions){
+        return(
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        );
+      }
+
+      
+      }
+    }
+  
+
+
+
---
+++ screens/WrittingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class WrittingScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>writting</Text>
+        </View>
+      );
+    }
+  }
