df114a3d522de5d1b8627b89aed98bdaf99ddcc8
Shivani2006 <60694698+Shivani2006@users.noreply.github.com>
2020-12-09 20:05:33 +0530

    stage 3

--- screens/ReadingScreen.js
+++ screens/ReadingScreen.js
@@ -1,107 +1,28 @@
 import React from 'react';
 import { Text, View, TouchableOpacity,TextInput,Image,StyleSheet } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+
 import { color } from 'react-native-reanimated';
 
 export default class ReadingScreen extends React.Component {
     constructor(){
       super();
       this.state = {
-        hasCameraPermissions: null,
-        scanned: false,
-        scannedBookId: '',
-        scannedReaderId:'',
-        buttonState: 'normal'
+       
       }
     }
 
-    getCameraPermissions = async (id) =>{
-      const {status} = await Permissions.askAsync(Permissions.CAMERA);
-      
-      this.setState({
-        /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
-        */
-        hasCameraPermissions: status === "granted",
-        buttonState: id,
-        scanned: false
-      });
-    }
+   
 
-    handleBarCodeScanned = async({type, data})=>{
-      const {buttonState} = this.state
-
-      if(buttonState==="BookId"){
-        this.setState({
-          scanned: true,
-          scannedBookId: data,
-          buttonState: 'normal'
-        });
-      }
-      else if(buttonState==="ReaderId"){
-        this.setState({
-          scanned: true,
-          scannedStudentId: data,
-          buttonState: 'normal'
-        });
-      }
-      
-    }
-
-    render() {
-      const hasCameraPermissions = this.state.hasCameraPermissions;
-      const scanned = this.state.scanned;
-      const buttonState = this.state.buttonState;
-
-      if (buttonState !== "normal" && hasCameraPermissions){
-        return(
-          <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
-          />
-        );
-      }
-
-      else if (buttonState === "normal"){
+    render() {    
+     
         return(
           <View style={styles.container}>
-            <View>
-              <Image
-                 style={{width:200, height: 200}}/>
-              <Text style={{textAlign: 'center', fontSize: 30}}>Wireles-library</Text>
-            </View>
-            <View style={styles.inputView}>
-            <TextInput 
-              style={styles.inputBox}
-              placeholder="Book Id"
-              value={this.state.scannedBookId}/>
-            <TouchableOpacity 
-              style={styles.scanButton}
-              onPress={()=>{
-                this.getCameraPermissions("BookId")
-              }}>
-              <Text style={styles.buttonText}>Scan</Text>
-            </TouchableOpacity>
-            </View>
-            <View style={styles.inputView}>
-            <TextInput 
-              style={styles.inputBox}
-              placeholder="Student Id"
-              value={this.state.scannedStudentId}/>
-            <TouchableOpacity 
-              style={styles.scanButton}
-              onPress={()=>{
-                this.getCameraPermissions("StudentId")
-              }}>
-              <Text style={styles.buttonText}>Scan</Text>
-            </TouchableOpacity>
-            </View>
+           
           </View>
         );
       }
     }
-  }
+  
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -109,39 +30,5 @@ export default class ReadingScreen extends React.Component {
       alignItems: 'center',
       color: 'dark blue'
     },
-    displayText:{
-      fontSize: 15,
-      fontFamily: 'century Gothic',
-      color: '#03befc',
-      textDecorationLine: 'underline'
-    },
-    scanButton:{
-      backgroundColor: '#2196F3',
-      padding: 10,
-      margin: 10
-    },
-    buttonText:{
-      fontSize: 15,
-      fontFamily: 'Bookman Old Style',
-      textAlign: 'center',
-      marginTop: 10
-    },
-    inputView:{
-      flexDirection: 'row',
-      margin: 20
-    },
-    inputBox:{
-      width: 200,
-      height: 40,
-      borderWidth: 1.5,
-      borderRightWidth: 0,
-      color: '#03fce3',
-      fontSize: 20
-    },
-    scanButton:{
-      backgroundColor: '#66BB6A',
-      width: 50,
-      borderWidth: 1.5,
-      borderLeftWidth: 0
-    }
+   
   });
--- screens/WrittingScreen.js
+++ screens/WrittingScreen.js
@@ -1,12 +1,122 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  TouchableHighlight,
+  Alert,
+  Image,
+  TextInput
+} from "react-native";
+import db from "../config";
+import firebase from "firebase";
+import { RFValue } from "react-native-responsive-fontsize";
+import MyHeader from "../components/MyHeader"
+
 
 export default class WrittingScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>writting</Text>
-        </View>
-      );
+    constructor(){
+      super();
+      this.state = {
+        userId: firebase.auth().currentUser.email,
+        name: '',
+        story:'',
+        
+      }
     }
-  }
+
+    addStory=(name, story)=>{
+      db.collection("stories").add({
+        user_id: this.state.userId,
+        name: name,
+        story: story,
+        story_id: Math.random().toString(36).substring(7),
+        
+      });
+      alert("Saved your idea")
+    }
+
+      render() {    
+     
+        return(
+          <View style={{ flex: 1 }}>
+            <View style={{ flex: 0.1 }}>
+              <MyHeader title= "Writer's idea" navigation={this.props.navigation} /> 
+            </View>
+              <View style={{ flex: 0.9, alignItems: "center" }}>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder={"Idea name"}
+               
+                onChangeText={(text) => {this.setState({name:text})}}
+                
+                value={this.state.name}
+              />
+               <TextInput
+                style={styles.formTextInput}                
+                multiline   
+                placeholder={"Tell something more about this Idea"}
+                onChangeText={(text) => {
+                  this.setState({
+                    story:text
+                  });
+                }}
+                value={this.state.story}
+              />
+
+               <TouchableOpacity
+                style={[styles.button, { marginTop: RFValue(30) }]}
+                onPress={() => {
+                  this.addStory(
+                    this.state.name,
+                    this.state.story
+                  );
+                }}
+              >
+                <Text
+                  style={styles.requestbuttontxt}
+                >
+                  Submit Idea
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      }
+    }
+  
+const styles = StyleSheet.create({
+ 
+  formTextInput: {
+    width: "75%",
+    height: RFValue(35),
+    borderWidth: 1,
+    padding: 10,
+    margin:10
+  },
+  requestbuttontxt:{
+    fontSize: RFValue(20),
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  button: {
+    width: "75%",
+    height: RFValue(60),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: RFValue(50),
+    backgroundColor: "#32867d",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+  },
+}) 
